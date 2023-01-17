5737ce1811a6bb99b2b7632c5d1e5337a98e91bc
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-17 15:30:19 -0600

    Updated NavBar.js to match design on Figma. NavBar is checking to see if the person on the site is logged in or not and will display a different navbar depending on the person's status

--- components/Signin.js
+++ components/Signin.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { signIn } from '../utils/auth';
+import NavBar from './NavBar';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        margin: '0 auto',
-        zIndex: 1,
-        minHeight: '25rem',
-        width: '100%',
-        minWidth: '30rem',
-        paddingBlock: '0 5rem',
-      }}
-    >
+    <div>
+      <NavBar />
       <h1>Hi there!</h1>
       <p>Click the button below to login!</p>
       <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
