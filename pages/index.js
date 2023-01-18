e14e7456a085cd2e87006850004c1467d497729d
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-17 20:56:25 -0600

    Added PaintingCards component to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image';
+import PaintingCards from '../components/cards/PaintingCards';
 import GalleryLogo from '../components/GalleryLogo';
 import NavBar from '../components/NavBar';
 
@@ -11,6 +12,10 @@ function Home() {
       <section>
         <Image src="/the-gallery.jpg" alt="Abstract Painting" width="2000" height="400" />
       </section>
+      <div>
+        <h3>What's selling?</h3>
+        <PaintingCards />
+      </div>
     </>
   );
 }
