9e296a1bf87772fea8c6d434d3d734e9e89d61c6
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-18 15:49:44 -0600

    Created and wrote the logic of what would render on the DOM for an individual's profile page

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,1 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import ProfileCard from '../../components/cards/ProfileCard';
+import { useAuth } from '../../utils/context/authContext';
+import getSingleUser from '../../utils/data/userData';
+
+export default function Profile() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { user } = useAuth();
+  const [userProfile, setUserProfile] = useState({});
+
+  const getUserDetails = () => {
+    getSingleUser(id, user.uid).then(setUserProfile);
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  }, []);
+
+  return (
+    <div>
+      <ProfileCard userProfileObj={userProfile} />
+    </div>
+  );
+}
