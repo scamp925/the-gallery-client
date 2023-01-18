50d467e03a25da8fecddb710972d4a39ee2034e7
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-17 22:09:28 -0600

    Added all of index.js logic to signin.js. There has to be a way to combine these files. Looking more into that next time.

--- components/Signin.js
+++ components/Signin.js
@@ -1,13 +1,34 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import ProductCards from './cards/ProductCards';
 import GalleryLogo from './GalleryLogo';
 import NavBar from './NavBar';
+import { getAllProducts } from '../utils/data/productData';
 
 function Signin() {
+  const [paintings, setPaintings] = useState([]);
+
+  const getAllPaintings = () => {
+    getAllProducts().then(setPaintings);
+  };
+
+  useEffect(() => {
+    getAllPaintings();
+  }, []);
+
   return (
     <>
       <NavBar />
+      <GalleryLogo />
+      <section>
+        <Image src="/the-gallery.jpg" alt="Abstract Painting" width="2000" height="400" />
+      </section>
       <div>
-        <GalleryLogo />
+        <h3>What's selling?</h3>
+        {paintings?.map((painting) => (
+          <ProductCards key={painting.id} paintingObj={painting} />
+        ))}
       </div>
     </>
   );
