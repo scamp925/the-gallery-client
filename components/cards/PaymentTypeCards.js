8f10ded3f92a7c50b858743ee2a186a2b1b64584
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-19 14:44:33 -0600

    Created PaymentType card to display the user's payment methods

---
+++ components/cards/PaymentTypeCards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+
+function PaymentTypeCards({ paymentObj }) {
+  return (
+    <div>
+      <p>{paymentObj.label}</p>
+      <p>{paymentObj.account_number}</p>
+      <div>
+        <Link href={`/recipes/edit/${paymentObj.id}`} passHref>
+          <Button variant="info" className="edit-btn">Edit</Button>
+        </Link>
+        <Button variant="danger" className="delete-btn">Delete</Button>
+      </div>
+    </div>
+  );
+}
+
+PaymentTypeCards.propTypes = {
+  paymentObj: PropTypes.shape({
+    id: PropTypes.number,
+    label: PropTypes.string,
+    account_number: PropTypes.number,
+    customer: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
+
+export default PaymentTypeCards;
