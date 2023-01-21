a3775653056f5dfd5107a3c2f531c7f165677677
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-21 14:13:40 -0600

    Cart is a link now and routes to cart page

---
+++ pages/cart/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GalleryLogo from '../../components/GalleryLogo';
+import NavBar from '../../components/NavBar';
+
+export default function Cart() {
+  return (
+    <div>
+      <NavBar />
+      <GalleryLogo />
+    </div>
+  );
+}
