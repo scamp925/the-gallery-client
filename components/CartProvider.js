fdd24b447ca8f465f103969fca0f0c888d8d7a9e
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-25 23:52:13 -0600

    Added another usage of useState to hold all the ids of the products in the cart. This will be needed to send to the backend when order is created

--- components/CartProvider.js
+++ components/CartProvider.js
@@ -5,8 +5,13 @@ export const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+  const [cartItemIds, setCartItemIds] = useState([]);
+
   return (
-    <CartContext.Provider value={{ cartItems, setCartItems }}>
+    <CartContext.Provider value={{
+      cartItems, setCartItems, cartItemIds, setCartItemIds,
+    }}
+    >
       {children}
     </CartContext.Provider>
   );
