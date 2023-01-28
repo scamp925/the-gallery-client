f0724e318143b61c72c028fde663e2a509603aa1
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-28 12:03:14 -0600

    VICTORY - finally got the registerForm not rendering the navbar with the cart, my account or all the paintings

--- components/NavBar.js
+++ components/NavBar.js
@@ -20,27 +20,44 @@ export default function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>All Paintings</Nav.Link>
-            </Link>
             {user && (
               <>
-                <NavDropdown title="My Account" id="basic-nav-dropdown">
-                  <Link passHref href={`users/${user.id}`}>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </Link>
-                  <Link passHref href="/orderHistory">
-                    <NavDropdown.Item>Order History</NavDropdown.Item>
-                  </Link>
-                  <NavDropdown.Divider />
-                  <Link passHref href="/">
-                    <NavDropdown.Item onClick={signOut}>Log Out</NavDropdown.Item>
-                  </Link>
-                </NavDropdown>
+                {user.username ? (
+                  <>
+                    <Link passHref href="/">
+                      <Nav.Link>All Paintings</Nav.Link>
+                    </Link>
+                    <NavDropdown title="My Account" id="basic-nav-dropdown">
+                      <Link passHref href={`users/${user.id}`}>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                      </Link>
+                      <Link passHref href="/orderHistory">
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                      </Link>
+                      <NavDropdown.Divider />
+                      <Link passHref href="/">
+                        <NavDropdown.Item onClick={signOut}>Log Out</NavDropdown.Item>
+                      </Link>
+                    </NavDropdown>
+                    <Link passHref href="/cart">
+                      <Nav.Link><FaShoppingCart size={26} /></Nav.Link>
+                    </Link>
+                  </>
+                ) : (
+                  <Nav>
+                    <Navbar.Text>Opps! Need to go back?</Navbar.Text>
+                    <Link passHref href="/">
+                      <Nav.Link onClick={signOut}>Back</Nav.Link>
+                    </Link>
+                  </Nav>
+                )}
               </>
             )}
             {!user && (
             <>
+              <Link passHref href="/">
+                <Nav.Link>All Paintings</Nav.Link>
+              </Link>
               <Link passHref href="/register">
                 <Nav.Link onClick={signIn}>Register</Nav.Link>
               </Link>
@@ -48,11 +65,11 @@ export default function NavBar() {
               <Link passHref href="/">
                 <Nav.Link onClick={signIn}>Log In</Nav.Link>
               </Link>
+              <Link passHref href="/cart">
+                <Nav.Link><FaShoppingCart size={26} /></Nav.Link>
+              </Link>
             </>
             )}
-            <Link passHref href="/cart">
-              <Nav.Link><FaShoppingCart size={26} /></Nav.Link>
-            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
